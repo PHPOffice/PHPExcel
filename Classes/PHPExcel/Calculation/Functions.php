3f613bba04ab7e216f8ed637cdda38e676546e9b
metfan <l_ulrich2002@yahoo.fr>
2013-04-29 14:36:31 +0200

    remove unnecessary break, probably old switch()

--- Classes/PHPExcel/Calculation/Functions.php
+++ Classes/PHPExcel/Calculation/Functions.php
@@ -579,7 +579,6 @@ class PHPExcel_Calculation_Functions {
 				return 4;
 		} elseif(is_array($value)) {
 				return 64;
-				break;
 		} elseif(is_string($value)) {
 			//	Errors
 			if ((strlen($value) > 0) && ($value{0} == '#')) {

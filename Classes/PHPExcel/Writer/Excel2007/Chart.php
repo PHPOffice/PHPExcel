09c6f5fc69beb80b978d78aa375f92cb3e0c3f8c
Simon Finne <simon.finne@loopon.com>
2013-06-15 20:55:00 +0200

    Rich text support for Chart Axis Labels for Excel2007

--- Classes/PHPExcel/Writer/Excel2007/Chart.php
+++ Classes/PHPExcel/Writer/Excel2007/Chart.php
@@ -449,17 +449,11 @@ class PHPExcel_Writer_Excel2007_Chart extends PHPExcel_Writer_Excel2007_WriterPa
 							$objWriter->endElement();
 
 							$objWriter->startElement('a:p');
-								$objWriter->startElement('a:r');
+								$caption = $xAxisLabel->getCaption();
+								if ((is_array($caption)) && (count($caption) > 0))
+									$caption = $caption[0];
 
-									$caption = $xAxisLabel->getCaption();
-									if (is_array($caption))
-										$caption = $caption[0];
-									$objWriter->startElement('a:t');
-//										$objWriter->writeAttribute('xml:space', 'preserve');
-										$objWriter->writeRawData(PHPExcel_Shared_String::ControlCharacterPHP2OOXML( $caption ));
-									$objWriter->endElement();
-
-								$objWriter->endElement();
+								$this->getParentWriter()->getWriterPart('stringtable')->writeRichTextForCharts($objWriter, $caption, 'a');
 							$objWriter->endElement();
 						$objWriter->endElement();
 					$objWriter->endElement();
@@ -575,17 +569,12 @@ class PHPExcel_Writer_Excel2007_Chart extends PHPExcel_Writer_Excel2007_WriterPa
 							$objWriter->endElement();
 
 							$objWriter->startElement('a:p');
-								$objWriter->startElement('a:r');
+								$caption = $yAxisLabel->getCaption();
+								if ((is_array($caption)) && (count($caption) > 0))
+									$caption = $caption[0];
 
-									$caption = $yAxisLabel->getCaption();
-									if (is_array($caption))
-										$caption = $caption[0];
-									$objWriter->startElement('a:t');
-//										$objWriter->writeAttribute('xml:space', 'preserve');
-										$objWriter->writeRawData(PHPExcel_Shared_String::ControlCharacterPHP2OOXML( $caption ));
-									$objWriter->endElement();
+								$this->getParentWriter()->getWriterPart('stringtable')->writeRichTextForCharts($objWriter, $caption, 'a');
 
-								$objWriter->endElement();
 							$objWriter->endElement();
 						$objWriter->endElement();
 					$objWriter->endElement();

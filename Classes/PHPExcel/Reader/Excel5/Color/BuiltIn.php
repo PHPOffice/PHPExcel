6d3a440aae68c1450dc93cd9a3535221ce13682f
MarkBaker <mark@lange.demon.co.uk>
2015-12-30 02:17:40 +0000

    Refactor colour mapping for Excel5 Reader

---
+++ Classes/PHPExcel/Reader/Excel5/Color/BuiltIn.php
@@ -0,0 +1,17 @@
+<?php
+
+class PHPExcel_Reader_Excel5_Color_BuiltIn
+{
+    public static $map = array(
+        0x00 => '000000',
+        0x01 => 'FFFFFF',
+        0x02 => 'FF0000',
+        0x03 => '00FF00',
+        0x04 => '0000FF',
+        0x05 => 'FFFF00',
+        0x06 => 'FF00FF',
+        0x07 => '00FFFF',
+        0x40 => '000000', // system window text color
+        0x41 => 'FFFFFF', // system window background color
+    );
+}
